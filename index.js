5d29e86cf7122c8fd9d17e3c8fb329b86501ec7d
Peninah Orengo <peninah.orengo@un.org>
2022-10-16 00:11:18 +0300

    html

---
+++ index.js
@@ -0,0 +1,38 @@
+const sections = document.querySelectorAll(".section");
+const sectBtns = document.querySelectorAll(".controls");
+const sectBtn = document.querySelectorAll(".control");
+const allSections = document.querySelectorAll(".main-content");
+
+function PageTransitions() {
+  //Button click active class
+  for (let i = 0; i < sectBtn.length; i++) {
+    sectBtn[i].addEventListener("click", function () {
+      let currentBtn = document.querySelectorAll(".active-btn");
+      currentBtn[0].className = currentBtn[0].className.replace(
+        "active-btn",
+        ""
+      );
+      this.className += " active-btn";
+    });
+  }
+
+  //sections Active
+  allSections.addEventListener("click", (e) => {
+    const id = e.target.dataset.id;
+    if (id) {
+      sectBtns.forEach((btn) => {
+        btn.ClassList.remove("active");
+      });
+      e.target.ClassList.add("active");
+
+      sections.forEach((section) => {
+        section.ClassList.remove("active");
+      });
+
+      const element = document.getElementById(id);
+      element.ClassList.add("active");
+    }
+  });
+}
+
+PageTransitions();
